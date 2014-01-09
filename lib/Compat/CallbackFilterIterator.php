aa39ceb920e17f0c1c85e536027c769bf562838b
Pierre Rineau <pierre.rineau@makina-corpus.com>
2014-01-09 13:11:41 +0100

    Added compat implementation of CallbackFilterIterator for PHP <5.4

---
+++ lib/Compat/CallbackFilterIterator.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Almost compatible with PHP 5.4 CallbackFilterIterator class.
+ */
+class CallbackFilterIterator extends \FilterIterator
+{
+    /**
+     * @var callback
+     */
+    private $callback;
+
+    /**
+     * Default constructor
+     *
+     * @param \Iterator $iterator
+     * @param callback $callback
+     */
+    public function __construct(\Iterator $iterator, $callback)
+    {
+        if (is_callable($callback)) {
+            $this->callback = $callback;
+        } else {
+            trigger_error("Given callback is not callable");
+        }
+
+        parent::__construct($iterator);
+    }
+
+    public function accept()
+    {
+        if (null !== $this->callback) {
+            return (bool)call_user_func(
+                $this->callback,
+                parent::current(),
+                parent::key(),
+                $this
+            );
+        }
+        return true;
+    }
+}
