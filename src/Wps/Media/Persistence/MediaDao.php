5730eae45a3d7c93c5132f3db369235481f5df3a
Pierre Rineau <pierre.rineau@makina-corpus.com>
2014-01-09 13:53:14 +0100

    Started development of Media dao

--- src/Wps/Media/Persistence/MediaDao.php
+++ src/Wps/Media/Persistence/MediaDao.php
@@ -2,14 +2,60 @@
 
 namespace Wps\Media\Persistence;
 
+use Wps\Media\Media;
+use Wps\Util\Date;
+
 use Smvc\Core\AbstractContainerAware;
+use Smvc\Error\NotFoundError;
 use Smvc\Error\NotImplementedError;
 
 class MediaDao extends AbstractContainerAware implements DaoInterface
 {
+    /**
+     * Create object from database result
+     *
+     * @param object $res
+     *   Database object result
+     *
+     * @return Media
+     */
+    protected function createObjectFrom($res)
+    {
+        $object = new Media();
+
+        $object->fromArray(array(
+            'id'          => $res->id,
+            'albumId'     => $res->id_album,
+            'accountId'   => $res->id_account,
+            'name'        => $res->name,
+            'path'        => $res->path,
+            'size'        => $res->size,
+            'width'       => $res->width,
+            'height'      => $res->height,
+            'userName'    => $res->user_name,
+            'md5Hash'     => $res->md5_hash,
+            'mimetype'    => $res->mimetype,
+            'addedDate'   => Date::fromTimestamp($res->ts_added),
+            'updatedDate' => Date::fromTimestamp($res->ts_updated),
+            'userDate'    => Date::fromTimestamp($res->ts_user_date),
+        ));
+
+        return $object;
+    }
+
     public function load($id)
     {
-        throw new NotImplementedError();
+        $db = $this->getContainer()->getDatabase();
+
+        $st = $db->prepare("SELECT * FROM media WHERE id = :id");
+        $st->setFetchMode(PDO::FETCH_OBJ);
+        $res = $st->execute(array(':id' => $id));
+
+        if (!$res) {
+            throw new NotFoundError();
+        }
+
+        return $this->createObjectFrom($res);
     }
 
     public function loadAll(array $idList)
@@ -24,7 +70,11 @@ class MediaDao extends AbstractContainerAware implements DaoInterface
 
     public function loadFirst(array $conditions)
     {
-        throw new NotImplementedError();
+        $ret = $this->loadAllFor($conditions, 1, 0);
+
+        if (!empty($ret)) {
+            return reset($ret);
+        }
     }
 
     public function save($object)
