710a399b9e90dbdd852cf1a94e7c420198fd8ead
Pierre Rineau <pierre.rineau@makina-corpus.com>
2014-01-14 14:01:58 +0100

    Minor improvements

--- src/Smvc/View/Helper/Template/Pager.php
+++ src/Smvc/View/Helper/Template/Pager.php
@@ -18,23 +18,25 @@ class Pager extends AbstractHelper
 
         $items = array();
         if (0 !== $currentPage) {
-            // Link to first
             $items[] = '<a href="' . $this->url('test', array('page' => $currentPage - 1)) . '" title="Go to first page">«</a>';
         }
         if (1 < $currentPage) {
-            // Link to previous
             $items[] = '<a href="' . $this->url('test', array('page' => $currentPage - 1)) . '" title="Go to previous page">‹</a>';
         }
+
         // @todo Page numbers
+
+        $items[] = '<a href="' . $this->url('test', array('page' => $currentPage)) . '" title="Current page">' . ($currentPage + 1) . '</a>';
+
+        // @todo Page numbers
+
         if ($currentPage < $pageCount - 1) {
-            // Link to next
             $items[] = '<a href="' . $this->url('test', array('page' => $pageCount - 1)) . '" title="Go to next page">›</a>';
         }
         if ($currentPage !== $pageCount) {
-            // Link to last
             $items[] = '<a href="' . $this->url('test', array('page' => $pageCount)) . '" title="Go to last page">»</a>';
         }
 
-        return '<ul class="pager"><li>' . implode('</li><li>', $items) . '</li></ul>';
+        return '<ul class="pager clear centered"><li>' . implode('</li><li>', $items) . '</li></ul>';
     }
 }
