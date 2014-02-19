f1319d9adfc1fe099cf72f06db6744aa9ece3dd4
Pierre Rineau <pierre.rineau@processus.org>
2014-02-19 22:31:05 +0100

    Added share form, needs form helper seriously, this is broken

--- src/Wps/Controller/App/Albums/ShareController.php
+++ src/Wps/Controller/App/Albums/ShareController.php
@@ -2,6 +2,8 @@
 
 namespace Wps\Controller\App\Albums;
 
+use Wps\Security\Access;
+
 use Smvc\Controller\AbstractController;
 use Smvc\Core\Message;
 use Smvc\Dispatch\RequestInterface;
@@ -11,6 +13,42 @@ use Smvc\View\View;
 
 class ShareController extends AbstractController
 {
+    public function getCurrentContacts($albumId)
+    {
+        $db = $this->getApplication()->getDatabase();
+
+        $st = $db->prepare("
+            SELECT a.id_account
+            FROM album_acl a
+            WHERE a.id_album = ?
+        ");
+        $st->setFetchMode(\PDO::FETCH_COLUMN, 0);
+        $st->execute(array($albumId));
+
+        return $st->fetchAll();
+    }
+
+    public function getCandidateContacts()
+    {
+        $app = $this->getApplication();
+        $db = $app->getDatabase();
+
+        $st = $db->prepare("
+            SELECT
+                c.id_contact,
+                COALESCE(a.user_name, a.mail)
+            FROM contact c
+            JOIN account a ON a.id = c.id_contact
+            WHERE c.id_account = ?
+        ");
+        $st->setFetchMode(\PDO::FETCH_KEY_PAIR);
+        $st->execute(array(
+            $app->getSession()->getAccount()->getId(),
+        ));
+
+        return $st->fetchAll();
+    }
+
     public function getAction(RequestInterface $request, array $args)
     {
         $app = $this->getApplication();
@@ -18,12 +56,16 @@ class ShareController extends AbstractController
         $contactDao = $app->getDao('contact');
         $album = $albumDao->load($args[0]);
 
-        // Already shared too
-
-        // Share potentials
-
         return new View(array(
             'album'  => $album,
+            'contacts' => $this->getCandidateContacts(),
+            'currentContacts' => $this->getCurrentContacts($album->getId()),
+            'accessLevel' => array(
+                Access::LEVEL_PRIVATE => "Private",
+                Access::LEVEL_FRIEND => "All my contacts",
+                Access::LEVEL_PUBLIC => "Public",
+            ),
+            'currentAccess' => $album->getAccessLevel(),
         ), 'app/album/share');
     }
 
@@ -32,24 +74,13 @@ class ShareController extends AbstractController
         if (1 !== count($args)) {
             throw new NotFoundError();
         }
-/*
-        $app = $this->getApplication();
-        $albumDao = $app->getDao('album');
-        $album = $albumDao->load($args[0]);
+
         $values = $request->getContent();
-
-        $data = array();
-
-        // @todo Filtering and other stuff
-        if (empty($values['userName'])) {
-            $data['userName'] = null;
-        } else {
-            $data['userName'] = $values['userName'];
+        $accesLevel = (int)$content['accessLevel'];
+        foreach ($values['contacts'] as $id => $value) {
+            
         }
 
-        $album->fromArray($data);
-        $albumDao->save($album);
-*/
         $app->getMessager()->addMessage("Album details have been updated", Message::TYPE_SUCCESS);
 
         return new RedirectResponse('app/albums/' . $album->getId());
